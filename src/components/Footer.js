fb2af16f27e188e04bc4cd86107384db12c4a093
Abd El Kader Seif El Islem RAHMANI <a.e.k426rahmani@gmail.com>
2025-04-12 10:05:13 +0100

    Create Footer component with copyright and social links

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <span className="copyright">Copyright &copy; Abd El Kader Seif El Islem RAHMANI 2025</span>
+          </div>
+          <div className="col-md-4 text-center">
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="https://wa.link/s3jbio"><i className="fa fa-whatsapp"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://github.com/RAHAMNIabdelkaderseifelislem"><i className="fa fa-github"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://www.linkedin.com/in/abd-el-kader-seif-el-islem-rahmani-2805b019b"><i className="fa fa-linkedin"></i></a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4 text-right">
+            <ul className="list-inline quicklinks">
+              <li className="list-inline-item"><a href="#">Privacy Policy</a></li>
+              <li className="list-inline-item"><a href="#">Terms of Use</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
