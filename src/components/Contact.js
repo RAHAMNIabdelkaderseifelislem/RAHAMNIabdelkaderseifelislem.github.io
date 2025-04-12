0bbe93794ee0f12b3f8629b447781f47ace591c5
Abd El Kader Seif El Islem RAHMANI <a.e.k426rahmani@gmail.com>
2025-04-12 10:04:47 +0100

    Create Contact component with contact information and links

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact" className="bg-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <h2 className="section-heading text-uppercase">Contact</h2>
+            <p>Feel free to contact me for any questions. For open-source projects, please open an issue or pull request on <a href="https://github.com/RAHAMNIabdelkaderseifelislem">GitHub</a>. If you want to follow my work, reach me on <a href="https://wa.link/s3jbio">WhatsApp</a>. Otherwise, send me an email at <a href="mailto:a.e.k426rahmani@gmail.com">a.e.k426rahmani@gmail.com</a>.</p>
+            <div className="text-center">
+              <a className="btn btn-primary btn-xl text-uppercase" href="https://github.com/RAHAMNIabdelkaderseifelislem">GitHub</a>
+              <a className="btn btn-primary btn-xl text-uppercase" href="https://wa.link/s3jbio">WhatsApp</a>
+              <a className="btn btn-primary btn-xl text-uppercase" href="mailto:a.e.k426rahmani@gmail.com">Email</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
