85368e07ba2eb00111e3e5b6c7e27b985c64e090
Abd El Kader Seif El Islem RAHMANI <a.e.k426rahmani@gmail.com>
2025-04-12 10:02:33 +0100

    Create About component with personal introduction

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about" className="bg-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 text-center">
+            <img className="img-fluid" src="img/team/rahmani.png" alt="AbdElKader Seif El Islem RAHMANI" style={{ marginBottom: '30px' }} />
+          </div>
+          <div className="col-lg-8 text-center text-md-left">
+            <h2>About Me</h2>
+            <p>Abd El Kader Seif El Islem RAHMANI is a highly driven and accomplished AI professional, currently pursuing a PhD in Artificial Intelligence at the University Centre of Naama — Salhi Ahmed. With a rich academic background, a passion for technological innovation, and a wealth of hands-on experience, Abd El Kader has made significant strides in the world of AI, machine learning, and data science.</p>
+            <p>His career is marked by a diverse array of cutting-edge projects, collaborations, and hackathon successes.</p>
+            <p>Abd El Kader’s journey into the world of AI began with his Bachelor’s and Master’s degrees from the University of Saida Dr. MOULAY Tahar, Algeria, where he demonstrated exceptional proficiency in data science, full-stack development, and machine learning. His academic achievements laid the foundation for his extensive work in AI engineering and data engineering, and have propelled him into the forefront of his field.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
